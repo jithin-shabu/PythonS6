08ae43cad9fff8e8c75cc0f96510e94967ff5e88
JIthin <jithinshabu1@gmail.com>
2025-04-14 11:42:30 +0530

    commit

---
+++ countletter.py
@@ -0,0 +1,8 @@
+str1="Hello Boys"
+count=0
+for x in str1:
+    if (x=='l'):
+        count=count+1
+if 'Hello' in str1:
+    print("Yes 'Hello' is present")
+print(count)
---
+++ test.py
@@ -0,0 +1,4 @@
+st=input("Enter the string")
+l=st.split()
+no=len(l)
+print("No of world is ",no)
